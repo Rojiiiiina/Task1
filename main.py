a785be55ac581d935c8f6f57420f319273404b31
Rojiiiiina <rojinamishra35@gmail.com>
2022-05-28 21:15:44 +0545

    This is my project

---
+++ main.py
@@ -0,0 +1,70 @@
+# list1 = [10,20,4,45,99]
+# print("Smallest number is:" , min(list1))
+
+# l1 = ["Hire", "the", "top", "1%", "freelancers"]
+#
+# l2 = []
+#
+# if len(l2):
+#     print("list is not empty")
+# else:
+#     print("list is empty")
+
+# import random
+# color_list = ['Red','Blue', 'Green', 'White', 'Black']
+# print(random.choice(color_list))
+
+# data = ["Sunil", "Roshan", 29]
+# data2 = []
+# data2 = data.copy()
+# print(data)
+# print(data2)
+
+# list1 = [10,20,4,45,99]
+# print("Second largest number is:", sorted(list1)[-2])
+
+# color = ['Red', 'Green', 'White','Blue','Brown']
+# color = [x for (i,x) in enumerate(color) if i not in (2,5)]
+# print(color)
+
+
+# list1 = [10,21,4,45,66,93,1]
+# even_count, odd_count = 0,0
+# for num in list1:
+#     if num % 2 == 0:
+#         even_count +=1
+#     else:
+#         odd_count +=1
+# print("Even numbers in the list:",even_count)
+# print("odd numbers in the list:",odd_count)
+
+# Tuple1=("ram","shyam","mohan")
+# print("original tuple", Tuple1)
+# List1= list(Tuple1)
+# print("original list",List1)
+# List1.insert(3,"krishna")
+# print("changed list",List1)
+# Tuple2=tuple(List1)
+# print("adding new name in tuple",Tuple2)
+
+# Tuple= (True,28,"Tiger")
+# List = list(Tuple)
+# print(type(List))
+# print(List)
+
+# tup = ('e','x','e','r','c','i','s','e')
+# str = ''.join(tup)
+# print(str)
+
+# def convert(list):
+#     return tuple(list)
+# list=[1,2,3,4]
+# print(convert(list))
+
+# ini_list = [{'a':[1,2,3],'b':[4,5,6]},{'c':[7,8,9],'d':[10,11,12]}]
+# dict_list = [(key, )+tuple(val)for dic in ini_list for key, val in dic.items()]
+# print("Resultant list of tuples:{}".format(dict_list))
+
+
+
+
